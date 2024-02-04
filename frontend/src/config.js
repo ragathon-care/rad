d1ccf57eebd077023ccf01a17d66b4b9ec670e55
shotamakino <45368165+shotamakino@users.noreply.github.com>
2024-02-04 20:02:43 +0000

    updates

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,11 @@
+const suggestedUrl = `https://${process.env.NEXT_PUBLIC_CODESPACE_NAME}-8000.app.github.dev/`;
 
 if (process.env.NEXT_PUBLIC_CODESPACES === 'true' && process.env.NEXT_PUBLIC_CODESPACE_NAME) {
-    const suggestedUrl = `https://${process.env.NEXT_PUBLIC_CODESPACE_NAME}-8000.app.github.dev/`;
+    console.log('found codespaces')
     if (!process.env.NEXT_PUBLIC_BACKEND_URL.startsWith(suggestedUrl)) {
         console.warn(`It looks like you're running on a Github codespace. You may want to set the NEXT_PUBLIC_BACKEND_URL environment variable to ${suggestedUrl}`);
     }
 }
 
-export const backendUrl = "https://rad-backend.onrender.com" // process.env.NEXT_PUBLIC_BACKEND_URL;
+export const backendUrl = process.env.NEXT_PUBLIC_CODESPACES ? suggestedUrl : process.env.NEXT_PUBLIC_BACKEND_URL;
 
