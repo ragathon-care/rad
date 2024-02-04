c03c3491ee26e1e95cb09af5028e86f5504121b0
shotamakino <45368165+shotamakino@users.noreply.github.com>
2024-02-04 19:06:55 +0000

    env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
         config.resolve.alias.canvas = false;
         return config;
     },
+    env: {
+        NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    }
 };
 
 export default nextConfig;
